bb20c410c8f0866dc2c7849b8e99ba2f91cb8ef7
Niclas Pirkl <129086200+ItzNicDev@users.noreply.github.com>
2024-11-12 10:56:28 +0100

    Create ELM327BT.h

---
+++ scripts/ELM327BT.h
@@ -0,0 +1,21 @@
+#ifndef ELM_327_BT_H
+#define ELM_327_BT_H
+
+#include <Arduino.h>
+#include "CANMessage.h"
+
+class ELM327BT {
+public:
+  //constructor
+  ELM327BT(char* bluetoothAdr);
+
+  //methods
+  void init();
+  char* requestValue(MessageType messageType);
+
+private:
+  char* _bluetoothAdr;
+  MessageType _messageType;  
+};
+
+#endif
