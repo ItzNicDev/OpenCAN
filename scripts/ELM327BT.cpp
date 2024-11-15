c9568ffb252a9e3df5594df32c2400de273ff8e9
Niclas Pirkl <129086200+ItzNicDev@users.noreply.github.com>
2024-11-15 11:05:54 +0100

    Update ELM327BT.cpp

--- scripts/ELM327BT.cpp
+++ scripts/ELM327BT.cpp
@@ -20,6 +20,35 @@ char* getResponse() {
   }
 }
 
+// EngineLoad,
+// CoolantTemperature,
+// FuelPressure,
+// IntakeManifoldAbsolutePressure,
+// EngineSpeed,
+// VehicleSpeed,
+// IntakeAirTemperature,
+
+double convertToUnit(int value, MessageType messageType) {
+
+  switch (messageType) {
+    case EngineLoad:
+      return value / 2.55;
+
+    case CoolantTemperature:
+      return value - 40;
+
+    case FuelPressure:
+      return 3 * value;
+
+    case IntakeManifoldAbsolutePressure:
+      return value;
+
+    case EngineSpeed:
+      return value / 4;
+  }
+}
+
+
 void ELM327BT::init() {
   BTSerial.begin(9600);
   BTSerial.println("AT+RESET");                          // Send AT+RESET command
@@ -28,7 +57,10 @@ void ELM327BT::init() {
 
 // Platzhalter für die Methode zum Anfragen eines Wertes
 char* ELM327BT::requestValue(MessageType messageType) {
+
   char* pid = canMessage.getPID(messageType);
+
   char* response = getResponse();
+
   return response;
 }
